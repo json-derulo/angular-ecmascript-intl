6ee63a7b2bd93980859f5c846c8109c11d48d6e4
Daniel Kimmich <json-derulo@users.noreply.github.com>
2023-08-05 09:25:37 +0200

    chore: use another prettier plugin to organize imports

--- projects/angular-ecmascript-intl/src/public-api.ts
+++ projects/angular-ecmascript-intl/src/public-api.ts
@@ -2,22 +2,22 @@
  * Public API Surface of angular-ecmascript-intl
  */
 
-export * from './lib/country/intl-country.pipe';
 export * from './lib/country/intl-country-pipe-default-options';
-export * from './lib/currency/intl-currency.pipe';
+export * from './lib/country/intl-country.pipe';
 export * from './lib/currency/intl-currency-pipe-default-options';
-export * from './lib/date/intl-date.pipe';
+export * from './lib/currency/intl-currency.pipe';
 export * from './lib/date/intl-date-pipe-default-options';
-export * from './lib/decimal/intl-decimal.pipe';
+export * from './lib/date/intl-date.pipe';
 export * from './lib/decimal/intl-decimal-pipe-default-options';
+export * from './lib/decimal/intl-decimal.pipe';
 export * from './lib/intl.module';
-export * from './lib/language/intl-language.pipe';
 export * from './lib/language/intl-language-pipe-default-options';
-export * from './lib/list/intl-list.pipe';
+export * from './lib/language/intl-language.pipe';
 export * from './lib/list/intl-list-pipe-default-options';
+export * from './lib/list/intl-list.pipe';
 export * from './lib/locale';
-export * from './lib/percent/intl-percent.pipe';
 export * from './lib/percent/intl-percent-pipe-default-options';
+export * from './lib/percent/intl-percent.pipe';
 export * from './lib/relative-time/relative-time.pipe';
-export * from './lib/unit/intl-unit.pipe';
 export * from './lib/unit/intl-unit-pipe-default-options';
+export * from './lib/unit/intl-unit.pipe';
