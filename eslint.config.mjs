af0ad903bc7efb65d475171150e229530c732ea0
Daniel Kimmich <json-derulo@users.noreply.github.com>
2024-11-26 18:48:53 +0100

    chore(deps): update angular eslint to v19

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,8 +33,7 @@ export default tseslint.config(
       "@typescript-eslint/no-deprecated": "error",
       "@typescript-eslint/no-unused-vars": "error",
       "@typescript-eslint/prefer-literal-enum-member": "off",
-      // ToDo: enable rule again once Angular ESLint adapted to v19 changes
-      // "@angular-eslint/prefer-standalone-component": "error",
+      "@angular-eslint/prefer-standalone": "error",
     },
   },
   {
