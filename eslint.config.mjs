0dc78bdba98b75b90e579cd99ae987befc420f22
Daniel Kimmich <json-derulo@users.noreply.github.com>
2026-03-03 19:23:10 +0100

    chore: migrate linting to oxlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,10 @@
-import js from "@eslint/js";
 import angularEslint from "angular-eslint";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ["**/dist/", "**/.angular/", "**/*.js", "**/coverage/"] },
-  {
-    files: ["**/*.ts"],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-      ...angularEslint.configs.tsRecommended,
-    ],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: [
-          "./libs/angular-ecmascript-intl/tsconfig.lib.json",
-          "./libs/angular-ecmascript-intl/tsconfig.spec.json",
-          "./demo/tsconfig.app.json",
-        ],
-      },
-      globals: {
-        ...globals.browser,
-      },
-    },
-    processor: angularEslint.processInlineTemplates,
-    rules: {
-      "@typescript-eslint/no-extraneous-class": "off",
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-deprecated": "error",
-      "@typescript-eslint/no-unused-vars": "error",
-      "@typescript-eslint/prefer-literal-enum-member": "off",
-      "@angular-eslint/prefer-standalone": "error",
-    },
-  },
-  {
-    files: ["**/*.spec.ts"],
-    languageOptions: {
-      globals: {
-        ...globals.jasmine,
-      },
-    },
-  },
+export default defineConfig(
+  { ignores: ["**/dist/", "**/.angular/", "**/coverage/"] },
   {
     files: ["**/*.html"],
     extends: [...angularEslint.configs.templateRecommended],
-    rules: {
-      "@angular-eslint/template/prefer-control-flow": "error",
-    },
   },
 );
